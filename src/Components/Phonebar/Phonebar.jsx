d3537c3663242cf883ff951f6469463d83f5949f
vatiza <rakibur.mac@gmail.com>
2024-02-25 03:24:43 +0600

    BarChart

--- src/Components/Phonebar/Phonebar.jsx
+++ src/Components/Phonebar/Phonebar.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 const Phonebar = () => {
@@ -23,11 +24,20 @@ const Phonebar = () => {
                     return phoneInfo;
                 });
                 console.log(phonesData);
+                setPhones(phonesData);
             }, [])
     });
     return (
         <div>
+            <BarChart width={1000} height={500} data={phones}>
+                <XAxis dataKey="name"></XAxis>
+                <YAxis dataKey="price"></YAxis>
+                <Bar dataKey="name" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                <Bar dataKey="price" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+                <Tooltip></Tooltip>
 
+
+            </BarChart>
         </div>
     );
 };
